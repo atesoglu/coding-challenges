ff8edab5ed05cc73dcab98ee694c3e06b8a0e9e2
Kadir Atesoglu <atesoglu@live.com>
2025-01-13 22:55:09 +0100

    TwoSum challenge.

---
+++ src/Solutions/LeetCode/Problems/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using Solutions.Base;
+
+namespace Solutions.LeetCode.Problems;
+
+public abstract class TwoSum : SolutionBase
+{
+    public static int[] Solve(int[] nums, int target)
+    {
+        var seenNumbers = new Dictionary<int, int>();
+
+        for (var index = 0; index < nums.Length; index++)
+        {
+            var complement = target - nums[index];
+
+            if (seenNumbers.TryGetValue(complement, out var number))
+            {
+                return [number, index];
+            }
+
+            if (!seenNumbers.ContainsKey(nums[index]))
+            {
+                seenNumbers[nums[index]] = index;
+            }
+        }
+
+        return [];
+    }
+}
---
+++ test/Tests/LeetCode/Problems/ProblemTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Solutions.LeetCode.Problems;
+
+namespace Tests.LeetCode.Problems;
+
+public class ProblemTests
+{
+    [Theory]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 3 }, 6, new[] { 0, 2 })]
+    public void TwoSumTest(int[] input, int target, int[] expected)
+    {
+        var output = TwoSum.Solve(input, target);
+
+        output.Should().BeEquivalentTo(expected);
+    }
+}
