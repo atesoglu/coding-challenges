46944cef497f6226db4939e8d38db02d5798b2ea
atesoglu <atesoglu@live.com>
2025-12-08 11:38:07 +0100

    Imlemented AoC 2025 Day 1 to Day 7 and 8 solutions.
    Also removed unnecessary days.

--- tests/AdventOfCode.Tests/Y2025/D07/Y2025D07.cs
+++ tests/AdventOfCode.Tests/Y2025/D07/Y2025D07.cs
@@ -3,7 +3,7 @@ using FluentAssertions;
 
 namespace AdventOfCode.Tests.Y2025.D07;
 
-[ChallengeName("Y2025D07ChallengeName")]
+[ChallengeName("Laboratories")]
 public class Y2025D07
 {
     private readonly string _input = File.ReadAllText(@"Y2025\D07\Y2025D07-input.txt", Encoding.UTF8);
@@ -11,16 +11,69 @@ public class Y2025D07
     [Fact]
     public void PartOne()
     {
-        var output = 0;
+        var output = Run(_input).splits;
 
-        output.Should().Be(0);
+        output.Should().Be(1555);
     }
 
     [Fact]
     public void PartTwo()
     {
-        var output = 0;
+        var output = Run(_input).timelineCount;
 
-        output.Should().Be(0);
+        output.Should().Be(12895232295789);
+    }
+
+
+    private (int splits, long timelineCount) Run(string input)
+    {
+        var grid = input
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.ToCharArray())
+            .ToArray();
+
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+
+        var current = new long[cols];
+        var totalSplits = 0;
+
+        for (var r = 0; r < rows; r++)
+        {
+            var next = new long[cols];
+
+            for (var c = 0; c < cols; c++)
+            {
+                var cell = grid[r][c];
+                var incoming = current[c];
+
+                switch (cell)
+                {
+                    case 'S':
+                        // A start creates a new timeline here.
+                        next[c] = 1;
+                        break;
+
+                    case '^':
+                        // Only count a split if there's something to split.
+                        if (incoming > 0)
+                            totalSplits++;
+
+                        // Safely send timelines left and right.
+                        if (c > 0) next[c - 1] += incoming;
+                        if (c + 1 < cols) next[c + 1] += incoming;
+                        break;
+
+                    default:
+                        // Normal cell: just pass the timeline downward.
+                        next[c] += incoming;
+                        break;
+                }
+            }
+
+            current = next;
+        }
+
+        return (totalSplits, current.Sum());
     }
 }
--- tests/AdventOfCode.Tests/Y2025/D08/Y2025D08.cs
+++ tests/AdventOfCode.Tests/Y2025/D08/Y2025D08.cs
@@ -3,7 +3,7 @@ using FluentAssertions;
 
 namespace AdventOfCode.Tests.Y2025.D08;
 
-[ChallengeName("Y2025D08ChallengeName")]
+[ChallengeName("Playground")]
 public class Y2025D08
 {
     private readonly string _input = File.ReadAllText(@"Y2025\D08\Y2025D08-input.txt", Encoding.UTF8);
@@ -11,16 +11,73 @@ public class Y2025D08
     [Fact]
     public void PartOne()
     {
-        var output = 0;
+        var points = Parse(_input);
+        var setOf = points.ToDictionary(p => p, p => new HashSet<Point>([p]));
+        foreach (var (a, b) in GetOrderedPairs(points).Take(1000))
+        {
+            if (setOf[a] != setOf[b])
+            {
+                Connect(a, b, setOf);
+            }
+        }
 
-        output.Should().Be(0);
+        var output = setOf.Values.Distinct()
+            .OrderByDescending(set => set.Count)
+            .Take(3)
+            .Aggregate(1, (a, b) => a * b.Count);
+
+        output.Should().Be(352584);
     }
 
     [Fact]
     public void PartTwo()
     {
-        var output = 0;
+        var points = Parse(_input);
+        var componentCount = points.Length;
+        var setOf = points.ToDictionary(p => p, p => new HashSet<Point>([p]));
+        var res = 0m;
+        foreach (var (a, b) in GetOrderedPairs(points).TakeWhile(_ => componentCount > 1))
+        {
+            if (setOf[a] != setOf[b])
+            {
+                Connect(a, b, setOf);
+                res = a.x * b.x;
+                componentCount--;
+            }
+        }
 
-        output.Should().Be(0);
+        var output = res;
+
+        output.Should().Be(9617397716);
     }
+
+    void Connect(Point a, Point b, Dictionary<Point, HashSet<Point>> setOf)
+    {
+        setOf[a].UnionWith(setOf[b]);
+        foreach (var p in setOf[b])
+        {
+            setOf[p] = setOf[a];
+        }
+    }
+
+    IEnumerable<(Point a, Point b)> GetOrderedPairs(Point[] points) =>
+        from a in points
+        from b in points
+        where (a.x, a.y, a.z).CompareTo((b.x, b.y, b.z)) < 0
+        orderby Metric(a, b)
+        select (a, b);
+
+    decimal Metric(Point a, Point b) =>
+        (a.x - b.x) * (a.x - b.x) +
+        (a.y - b.y) * (a.y - b.y) +
+        (a.z - b.z) * (a.z - b.z);
+
+    Point[] Parse(string input) => (
+        from line in input.Split("\n")
+        let parts = line.Split(",").Select(int.Parse).ToArray()
+        select new Point(parts[0], parts[1], parts[2])
+    ).ToArray();
+
+
+    record Point(decimal x, decimal y, decimal z);
 }
