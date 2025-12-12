9624ed71f139073a3761a664d186f26a0fca73ee
atesoglu <atesoglu@live.com>
2025-12-12 10:20:42 +0100

    Imlemented AoC 2025 Day 12 solutions.

--- tests/AdventOfCode.Tests/Y2025/D12/Y2025D12.cs
+++ tests/AdventOfCode.Tests/Y2025/D12/Y2025D12.cs
@@ -1,9 +1,10 @@
 using System.Text;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 
 namespace AdventOfCode.Tests.Y2025.D12;
 
-[ChallengeName("Y2025D12ChallengeName")]
+[ChallengeName("Christmas Tree Farm")]
 public class Y2025D12
 {
     private readonly string _input = File.ReadAllText(@"Y2025\D12\Y2025D12-input.txt", Encoding.UTF8);
@@ -11,16 +12,53 @@ public class Y2025D12
     [Fact]
     public void PartOne()
     {
+        var blocks = _input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var treeBlocks = blocks[..^1];
+        var todoBlock = blocks[^1];
+
+        var areas = treeBlocks.Select(b => b.Count(c => c == '#')).ToArray();
+
+        var todos = ParseTodos(todoBlock);
+
         var output = 0;
 
-        output.Should().Be(0);
+        foreach (var todo in todos)
+        {
+            var requiredArea = 0;
+
+            for (var i = 0; i < todo.Counts.Length; i++)
+            {
+                requiredArea += todo.Counts[i] * areas[i];
+            }
+
+            if (requiredArea <= todo.Width * todo.Height)
+            {
+                output++;
+            }
+        }
+
+        output.Should().Be(521);
     }
 
-    [Fact]
-    public void PartTwo()
+    private static Todo[] ParseTodos(string block)
     {
-        var output = 0;
+        var lines = block.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        var todos = new List<Todo>();
 
-        output.Should().Be(0);
+        foreach (var line in lines)
+        {
+            var nums = Regex.Matches(line, @"\d+").Select(m => int.Parse(m.Value)).ToArray();
+
+            var w = nums[0];
+            var h = nums[1];
+            var counts = nums.Skip(2).ToArray();
+
+            todos.Add(new Todo(w, h, counts));
+        }
+
+        return todos.ToArray();
     }
+
+    private record Todo(int Width, int Height, int[] Counts);
 }
